b8d073fe4a78161a7701280eedffa9961c85c196
Basher006 <ifyryk@gmail.com>
2024-03-26 16:02:33 +0500

    Global Exeption hook!

--- Drinker/Program.cs
+++ Drinker/Program.cs
@@ -6,25 +6,27 @@ namespace Drinker
     public static class Program
     {
         public static Form1 DrinkerForm = new Form1();
-        [STAThread] static void Main()
+        [STAThread]
+        static void Main()
         {
             Log.Write("\n============================\nApp start\n============================");
 
-            try
-            {
-                ApplicationConfiguration.Initialize();
-                Application.Run(DrinkerForm);
-            }
-            catch (Exception e)
-            {
-                Log.Write(e.ToString(), Log.LogType.Error);
-                throw;
-            }
-            finally
-            {
-                BotKeyHook.DispouseHook();
-                Log.Write("Application Close.");
-            }
+            ApplicationConfiguration.Initialize();
+            Application.ThreadException += new ThreadExceptionEventHandler(GlobalExeption);
+            Application.ApplicationExit += OnAppExit;
+            Application.Run(DrinkerForm);
+        }
+
+        private static void OnAppExit(object? sender, EventArgs e)
+        {
+            BotKeyHook.DispouseHook();
+            Log.Write("Application Close.");
+        }
+
+        private static void GlobalExeption(object sender, ThreadExceptionEventArgs e)
+        {
+            Log.Write(e.Exception.ToString(), Log.LogType.Error);
+            Application.Exit();
         }
     }
 }
